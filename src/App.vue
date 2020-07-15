0a5f47d54f947fb5af0e6b102df475918f4ca7af
LyrranKelmalu <45206026+LyrranKelmalu@users.noreply.github.com>
2020-07-15 18:59:55 +0300

    Add files via upload
    first upload (start of project)

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/my_photo.jpg">
+    <HelloWorld msg="Welcome to my porfolio"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/mainPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+  img{
+    border-radius: 100%;
+    width: 250px;
+    height: 250px;
+  }
+</style>
---
+++ src/components/mainPage.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      My name is Roman and I am 29 years old. <br>
+      I am a beginner web developer.<br>
+      I'm studying at college as a system administrator.
+    </p>
+    <h3>My skills</h3>
+    <ul>
+      <li><img src="" alt="">HTML</li>
+      <li><img src="" alt="">CSS(SCSS/SASS)</li>
+      <li><img src="" alt="">JS(JQUERY, VUE.JS)</li>
+      <li><img src="" alt="">PHP</li>
+      <li><img src="" alt="">MySQL</li>
+    </ul>
+    <h3>My Git</h3>
+    <ul>
+      <li><a href="https://github.com/LyrranKelmalu/" target="_blank" rel="noopener">LyrranKelmalu</a></li>
+    </ul>
+    <h3>Contact Links</h3>
+    <ul>
+      <li><a href="https://twitter.com/LyrranK" target="_blank" rel="Twitter">Twitter</a></li>
+      <li><a href="https://www.instagram.com/lyrran_kelmalu/" target="_blank" rel="insta">Instagram</a></li>
+      <li><a href="https://vk.com/lyrra" target="_blank" rel="vk">VK</a></li>
+      <li><a href="https://www.facebook.com/roman.pappinen.1/" target="_blank" rel="noopener">FaceBook</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
